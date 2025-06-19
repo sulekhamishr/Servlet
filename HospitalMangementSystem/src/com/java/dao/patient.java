396b389c017b345bda6b2acc0b5fc8933e9821b2
sulekhamishr <msulekha277@gmail.com>
2025-06-19 15:44:44 +0530

    Add files via upload

---
+++ HospitalMangementSystem/src/com/java/dao/patient.java
@@ -0,0 +1,22 @@
+package com.java.dao;
+
+public interface patient {
+
+    private int patientId;
+    private String name;
+    private Date dateOfBirth;
+    private String gender;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    private String medicalHistory;
+    private String bloodGroup;
+    private String insuranceDetails;
+    private String emergencyContact;
+    private Date admissionDate;
+    private String status;
+    private int doctorId;
+    
+    
+	
+}
---
+++ HospitalMangementSystem/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module HospitalMangementSystem {
+}
